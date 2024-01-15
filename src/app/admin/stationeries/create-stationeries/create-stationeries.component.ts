ceceb50bf0a3888347a7cdeeceb75f870e5ee244
trkhduy <trkhduy003@gmail.com>
2024-01-15 14:13:56 +0700

    EP-4 EF-7: feat create stationery, role layout

--- src/app/admin/stationeries/create-stationeries/create-stationeries.component.ts
+++ src/app/admin/stationeries/create-stationeries/create-stationeries.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-create-stationeries',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-stationeries.component.scss']
 })
 export class CreateStationeriesComponent {
-
+  public Editor = ClassicEditor;
 }
