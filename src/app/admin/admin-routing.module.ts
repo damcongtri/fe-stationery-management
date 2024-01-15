c6e488944331146ff5db5d799a2ef97ab79e07f2
minhka <minhka192@gmail.com>
2024-01-15 14:25:53 +0700

    update layout

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BannerCreateComponent } from './banner-management/banner-create/banner-create.component';
-import { ProductsComponent } from './products/products.component';
-
 const routes: Routes = [
     {
         path: '',
@@ -12,10 +10,7 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Chuyển hướng ngay lập tức đến dashboard
             { path: '', component: DashboardComponent },
-            {
-                path: 'product',
-                loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
-            },
+            
             {
                 path: 'category',
                 loadChildren: () => import('./category/category.module').then(m => m.CategoryModule)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     { path: '', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
     // { path: '', redirectTo: 'admin', pathMatch: 'full' }, // Chuyển hướng trang gốc về '/admin'
-    { path: '', loadChildren: () => import('./customer/customer.module').then((m) => m.CustomerModule) },
+    // { path: '', loadChildren: () => import('./customer/customer.module').then((m) => m.CustomerModule) },
 ];
 
 @NgModule({
